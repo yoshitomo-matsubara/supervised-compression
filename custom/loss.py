ad54b917e70345f0e6f24a8ed46b746e0113fc93
Yoshitomo Matsubara <yoshitom@uci.edu>
2021-02-15 12:05:12 -0800

    temporarily hardcoded original loss function for segmentation models

--- custom/loss.py
+++ custom/loss.py
@@ -11,6 +11,21 @@ def extract_org_loss_dict(org_criterion, student_outputs, teacher_outputs, targe
     return org_loss_dict
 
 
+@register_func2extract_org_output
+def extract_org_segment_loss(org_criterion, student_outputs, teacher_outputs, targets, uses_teacher_output, **kwargs):
+    org_loss_dict = dict()
+    if isinstance(student_outputs, dict):
+        sub_loss_dict = dict()
+        for key, outputs in student_outputs.items():
+            sub_loss_dict[key] = org_criterion(outputs, targets)
+
+        org_loss = sub_loss_dict['out']
+        if 'aux' in sub_loss_dict:
+            org_loss += 0.5 * sub_loss_dict['aux']
+        org_loss_dict.update(org_loss)
+    return org_loss_dict
+
+
 @register_single_loss
 class BppBasedLoss(nn.Module):
     def __init__(self, compressor_module_path, beta, reduction='mean'):
