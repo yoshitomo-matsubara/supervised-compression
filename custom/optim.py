b47897ca83b4984d95a9203b98c65761d89dcf09
Yoshitomo Matsubara <yoshitom@uci.edu>
2021-02-18 21:58:09 -0800

    fixed bugs

--- custom/optim.py
+++ custom/optim.py
@@ -3,7 +3,7 @@ from torchdistill.common.func_util import register_scheduler
 
 
 @register_scheduler
-def custom_lambda_lr(optimizer, dataset_size, num_epochs, factor=0.9):
+def custom_lambda_lr(optimizer, num_iterations, num_epochs, factor=0.9):
     lr_scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer,
-                                                     lambda x: (1 - x / (dataset_size * num_epochs)) ** factor)
+                                                     lambda x: (1 - x / (num_iterations * num_epochs)) ** factor)
     return lr_scheduler
