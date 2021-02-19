d5cdba38d2640babdbbb6acf45a2989037db8fdb
Yoshitomo Matsubara <yoshitom@uci.edu>
2021-02-18 22:34:56 -0800

    simplified

--- custom/optim.py
+++ custom/optim.py
@@ -1,9 +1,8 @@
-import torch
+from torch.optim.lr_scheduler import LambdaLR
 from torchdistill.common.func_util import register_scheduler
 
 
 @register_scheduler
 def custom_lambda_lr(optimizer, num_iterations, num_epochs, factor=0.9):
-    lr_scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer,
-                                                     lambda x: (1 - x / (num_iterations * num_epochs)) ** factor)
+    lr_scheduler = LambdaLR(optimizer, lambda x: (1 - x / (num_iterations * num_epochs)) ** factor)
     return lr_scheduler
