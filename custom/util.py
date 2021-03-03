3e39672b703fa328a8995c81e850657539e085e0
Yoshitomo Matsubara <yoshitom@uci.edu>
2021-03-02 22:11:00 -0800

    extended for detection and segmentation

--- custom/util.py
+++ custom/util.py
@@ -4,28 +4,61 @@ import torch
 from torchdistill.common import file_util, module_util
 from torchdistill.common.constant import def_logger
 
-from custom.model import BottleneckResNet
 
 logger = def_logger.getChild(__name__)
 
 
+def check_if_module_exits(module, module_path):
+    module_names = module_path.split('.')
+    child_module_name = module_names[0]
+    if len(module_names) == 1:
+        return hasattr(module, child_module_name)
+
+    if not hasattr(module, child_module_name):
+        return False
+    return check_if_module_exits(getattr(module, child_module_name), '.'.join(module_names[1:]))
+
+
 def load_bottleneck_model_ckpt(model, ckpt_file_path):
+    from custom.model import BottleneckResNet
     if not file_util.check_if_exists(ckpt_file_path):
         return False
 
-    # For classifier
+    ckpt = torch.load(ckpt_file_path, map_location='cpu')
     if isinstance(model, BottleneckResNet):
-        logger.info('Loading entropy bottleneck parameters')
-        ckpt = torch.load(ckpt_file_path, map_location='cpu')
+        # For classifier
+        logger.info('Loading entropy bottleneck parameters for classifier')
         model_ckpt = ckpt['model']
-        eb_state_dict = OrderedDict()
+        entropy_bottleneck_state_dict = OrderedDict()
         for key in list(model_ckpt.keys()):
             if key.startswith('backbone.bottleneck_layer.'):
-                eb_state_dict[key.replace('backbone.bottleneck_layer.', '')] = model_ckpt.pop(key)
+                entropy_bottleneck_state_dict[key.replace('backbone.bottleneck_layer.', '')] = model_ckpt.pop(key)
 
         model.load_state_dict(model_ckpt, strict=False)
-        model.backbone.bottleneck_layer.load_state_dict(eb_state_dict)
+        model.backbone.bottleneck_layer.load_state_dict(entropy_bottleneck_state_dict)
         return True
+    elif check_if_module_exits(model, 'backbone.body.bottleneck_layer'):
+        # For detector
+        logger.info('Loading entropy bottleneck parameters for detector')
+        model_ckpt = ckpt['model']
+        entropy_bottleneck_state_dict = OrderedDict()
+        for key in list(model_ckpt.keys()):
+            if key.startswith('backbone.body.bottleneck_layer.'):
+                entropy_bottleneck_state_dict[key.replace('backbone.body.bottleneck_layer.', '')] = model_ckpt.pop(key)
+
+        model.load_state_dict(model_ckpt, strict=False)
+        model.backbone.body.bottleneck_layer.load_state_dict(entropy_bottleneck_state_dict)
+    elif check_if_module_exits(model, 'backbone.bottleneck_layer'):
+        # For segmenter
+        logger.info('Loading entropy bottleneck parameters for segmenter')
+        model_ckpt = ckpt['model']
+        entropy_bottleneck_state_dict = OrderedDict()
+        for key in list(model_ckpt.keys()):
+            if key.startswith('backbone.bottleneck_layer.'):
+                entropy_bottleneck_state_dict[key.replace('backbone.bottleneck_layer.', '')] = model_ckpt.pop(key)
+
+        model.load_state_dict(model_ckpt, strict=False)
+        model.backbone.bottleneck_layer.load_state_dict(entropy_bottleneck_state_dict)
     return False
 
 
